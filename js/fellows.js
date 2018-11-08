369cdb6c1014ba68804a22cf12ac0b97a93fb437
Sam Beckbessinger <bcksam002@gmail.com>
2018-11-08 16:29:14 +0400

    update post design guidelines finalised

---
+++ js/fellows.js
@@ -0,0 +1,10 @@
+
+/* Open when someone clicks on the span element */
+function openNav(fellow) {
+    document.getElementById(fellow).style.width = "100%";
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeNav(fellow) {
+    document.getElementById(fellow).style.width = "0%";
+}
